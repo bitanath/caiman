259113695195580bc13b82ec569fd5f77dd0554b
bitanath <bitanath@gmail.com>
2024-08-02 14:41:39 +0530

    Shell Script for Granting permisisons

---
+++ backend/permissions.sh
@@ -0,0 +1,14 @@
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess adminClientEmail
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess adminPrivateKey
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess authCookieName
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess authCookieSignatureCurrent
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess authCookieSignaturePrevious
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseApiKey
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseAuthDomain
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseAuthToken
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseCertX509
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseClientId
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseDbUrl
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseProjectId
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess firebaseSenderId
+firebase --project caiman-75133 --backend caiman apphosting:secrets:grantaccess useSecureCookies
